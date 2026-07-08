71a1b00d26b64e4126a33b566f0edca931b33214
lzjc2024 <lzjc1613@outlook.com>
2026-07-08 19:39:11 +0800

    index.html

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,1 @@
-let res = ["https://5ac9cbb9.r39.cpolar.top","Wed Jul 08 2026 19:08:17 GMT+0800 (中国标准时间)"]
+let res = ["https://5ac9cbb9.r39.cpolar.top","Wed Jul 08 2026 19:39:11 GMT+0800 (中国标准时间)"]
