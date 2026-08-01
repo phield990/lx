fda19b72208df3c306fdf48f97a4d7a240a4c93c
lzjc2024 <lzjc1613@outlook.com>
2026-08-02 05:19:58 +0800

    index.html

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,1 @@
-let res = ["https://5a5a5f87.r39.cpolar.top","Sun Aug 02 2026 04:49:06 GMT+0800 (中国标准时间)"]
+let res = ["https://5a5a5f87.r39.cpolar.top","Sun Aug 02 2026 05:19:57 GMT+0800 (中国标准时间)"]
